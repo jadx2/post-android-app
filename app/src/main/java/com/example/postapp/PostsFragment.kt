6f13bd4670ff0a7f9f45774ffdb6ef76ef9ea617
jadx2 <delmarjaim@gmail.com>
2021-11-01 08:22:30 -0500

    Set up data binding and create posts fragment

---
+++ app/src/main/java/com/example/postapp/PostsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.postapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.postapp.databinding.FragmentPostsBinding
+
+class PostsFragment : Fragment() {
+    lateinit var binding: FragmentPostsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_posts, container, false)
+        return binding.root
+    }
+}
