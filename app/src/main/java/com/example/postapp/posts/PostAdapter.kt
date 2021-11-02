5b118175e50f3f40367395405bf610cfd554fce8
jadx2 <delmarjaim@gmail.com>
2021-11-02 17:55:39 -0500

    Implement recyclerView for the posts

--- app/src/main/java/com/example/postapp/posts/PostAdapter.kt
+++ app/src/main/java/com/example/postapp/posts/PostAdapter.kt
@@ -1,1 +1,48 @@
 package com.example.postapp.posts
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.postapp.database.poststable.Post
+import com.example.postapp.databinding.PostsListItemBinding
+import com.example.postapp.posts.PostsAdapter.ViewHolder
+
+class PostsAdapter: ListAdapter<Post, ViewHolder>(PostsDiffCallback())  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.postTitle.text = item.title.toString()
+        holder.postBody.text = item.body.toString()
+    }
+
+
+    class ViewHolder private constructor(val binding: PostsListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val postTitle: TextView = binding.postTitle
+        val postBody: TextView = binding.postBody
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = PostsListItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
+
+class PostsDiffCallback: DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/com/example/postapp/posts/PostsFragment.kt
+++ app/src/main/java/com/example/postapp/posts/PostsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.postapp.posts
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.postapp.R
 import com.example.postapp.database.PostsDatabase
@@ -26,6 +28,13 @@ class PostsFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)[PostsViewModel::class.java]
         binding.lifecycleOwner = this
         binding.postsViewModel = viewModel
+        val adapter = PostsAdapter()
+        binding.postsList.adapter = adapter
+        viewModel.posts.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
         return binding.root
     }
 
